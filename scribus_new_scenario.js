037c7586a283f469fcaf7e40b1f8f6bfaf41686f
gbarkovalev <42040437+gbarkovalev@users.noreply.github.com>
2018-09-23 08:07:02 +0300

    Add files via upload

---
+++ scribus_new_scenario.js
@@ -0,0 +1,19 @@
+var a;
+a = 28;
+
+
+
+function new_scenario()
+{
+  var iAmWell;
+  iAmWell="1";
+  if (iAmWell)
+ 
+  {
+  iAmWell=1;
+  console.log('you are healthy');
+  }
+
+}
+
+new_scenario();
